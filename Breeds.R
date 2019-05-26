53acb8b6e8ec11f52656a5b3744a65b29bdc1ae8
jzushi <joshuazushi13@gmail.com>
2019-05-26 17:55:37 -0600

    Breed list

--- Breeds.R
+++ Breeds.R
@@ -1,10 +1,22 @@
+# Load Packages
+
 library(tidyverse)
 library(dplyr)
+
+# Load Data
+
 df <-  read.csv("../fci-breeds/fci-breeds.csv")
 glimpse(df)
+
+# Chose what I wanted to work with 
+
 id_num <- as.character(df$id)
 breed_name <-  as.character(df$name)
 breed_section <- as.character(df$section)
 df2 <- as.data.frame(breed_name, id_num)
 df2$breed_section <- breed_section
+
+# Created the lsit
+
 breed_list <- df2
+breed_list
